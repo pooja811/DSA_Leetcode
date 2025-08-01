c29700dbc5798b7260c4a5c947c573d182461db6
pooja811 <142797724+pooja811@users.noreply.github.com>
2025-08-01 16:29:08 +0530

    Time: 1 ms (99.28%) | Memory: 44.6 MB (92.01%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int [] arr = new int[26];
+        for(char c : ransomNote.toCharArray()){
+            arr[c-'a']++;
+        }
+
+        for(char c : magazine.toCharArray()){
+            arr[c-'a']--;
+        }
+
+        for(int i : arr){
+            if(i>0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
