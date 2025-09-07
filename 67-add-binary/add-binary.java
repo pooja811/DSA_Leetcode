74edaafab9c0d60be354a80eb5cbf9dd6d4655e1
Pooja More <142797724+pooja811@users.noreply.github.com>
2025-09-07 20:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (52.18%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,21 +1,28 @@
-class Solution 
-{
-  public String addBinary(String a, String b) 
-  {
-    StringBuilder sb = new StringBuilder();
-    int carry = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
+class Solution{
+    public String addBinary(String a, String b ){
+        int lenA = a.length();
+        int lenB = b.length();
 
-    while (i >= 0 || j >= 0 || carry == 1) 
-    {
-      if(i >= 0)
-        carry += a.charAt(i--) - '0';
-      if(j >= 0)
-        carry += b.charAt(j--) - '0';
-      sb.append(carry % 2);
-      carry /= 2;
-    }
-    return sb.reverse().toString();
-  }
+        if(lenA<lenB)return addBinary(b,a);
+
+        char[] result = new char[lenA +1];
+        int carry = 0;
+        int k= lenA;
+for (int i = lenA - 1, j = lenB - 1; i >= 0; i--) {
+        // for(int i= lenA -1; j = lenB-1;i>=0; i--){
+            carry += a.charAt(i)-'0';
+            if(j>=0){
+                carry+=b.charAt(j--) - '0';
+
+            }
+            result[k--]= (char)('0'+(carry & 1));
+            carry>>=1;
+        }
+
+        if(carry == 1){
+            result[k]='1';
+            return new String(result);
+        }
+        return new String(result,1, lenA);
+         }
 }
