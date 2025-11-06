7074e778d0703584200fb5ef43cbf5150d2cb544
Pooja More <142797724+pooja811@users.noreply.github.com>
2025-11-06 22:25:43 +0530

    Time: 7 ms (62.77%) | Memory: 49.5 MB (10.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        
-        for (String word : strs) {
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String sortedWord = new String(chars);
-            
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
-            }
-            
-            map.get(sortedWord).add(word);
+
+        for(int i = 0; i < strs.length; i++){
+            char[] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String sortedWord = new String(charArray);
+            List<String> temp = map.getOrDefault(sortedWord, new ArrayList<>());
+            temp.add(strs[i]);
+            map.put(sortedWord, temp);
         }
-        
         return new ArrayList<>(map.values());
     }
 }
