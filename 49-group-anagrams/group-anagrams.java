6b9d22c23af06447c119f74363217d38475482f5
Pooja More <142797724+pooja811@users.noreply.github.com>
2025-11-07 20:20:39 +0530

    Time: 6 ms (98.55%) | Memory: 49.9 MB (5.80%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,15 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for(int i = 0; i < strs.length; i++){
-            char[] charArray = strs[i].toCharArray();
-            Arrays.sort(charArray);
-            String sortedWord = new String(charArray);
-            List<String> temp = map.getOrDefault(sortedWord, new ArrayList<>());
-            temp.add(strs[i]);
-            map.put(sortedWord, temp);
+        Map<String,List<String>> anagrams = new HashMap<>();
+        List<List<String>> ans = new ArrayList<>();
+        for(String str : strs){
+            char[] chArray = str.toCharArray();
+            Arrays.sort(chArray);
+            String sortedStr = new String(chArray);
+            if(!anagrams.containsKey(sortedStr)){
+                 List<String> list = new ArrayList<>();
+                 list.add(str);
+                 anagrams.put(sortedStr,list);
+            }else{
+               anagrams.get(sortedStr).add(str);
+            }
         }
-        return new ArrayList<>(map.values());
+        
+        for(List<String> list : anagrams.values()){
+            ans.add(list);
+        }
+        return ans;
     }
 }
+/**
+Time : n*O(m(logm))
+n is number of strings in array
+m is length of each string
+
+Space : O(n*m)
+
+ */
