9466fb0d0465ee9d65e599402239980d0612d09c
Pooja More <142797724+pooja811@users.noreply.github.com>
2025-11-25 11:25:06 +0530

    Time: 3 ms (90.87%) | Memory: 42.8 MB (42.32%) - LeetSync

--- 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
@@ -1,56 +1,37 @@
-import java.math.BigInteger;
-
 class Solution {
-    public String fractionAddition(String expression) {
-        BigInteger num = BigInteger.ZERO;
-        BigInteger den = BigInteger.ONE;
-        int i = 0;
-        int n = expression.length();
+    public String fractionAddition(String exp) {
+        int numer = 0, denom = 1;
+        int i = 0, n = exp.length();
         
         while (i < n) {
             int sign = 1;
-            
-            // Determine the sign of the current fraction
-            if (expression.charAt(i) == '-') {
-                sign = -1;
-                i++;
-            } else if (expression.charAt(i) == '+') {
+            if (exp.charAt(i) == '+' || exp.charAt(i) == '-') {
+                sign = (exp.charAt(i) == '-') ? -1 : 1;
                 i++;
             }
             
-            // Extract the numerator
-            int j = i;
-            while (j < n && Character.isDigit(expression.charAt(j))) {
-                j++;
+            int num = 0;
+            while (i < n && Character.isDigit(exp.charAt(i))) {
+                num = num * 10 + (exp.charAt(i++) - '0');
             }
-            BigInteger numerator = new BigInteger(expression.substring(i, j)).multiply(BigInteger.valueOf(sign));
+            num *= sign;
             
-            // Move past the '/' character
-            i = j + 1;
+            i++; // skip '/'
             
-            // Extract the denominator
-            j = i;
-            while (j < n && Character.isDigit(expression.charAt(j))) {
-                j++;
+            int den = 0;
+            while (i < n && Character.isDigit(exp.charAt(i))) {
+                den = den * 10 + (exp.charAt(i++) - '0');
             }
-            BigInteger denominator = new BigInteger(expression.substring(i, j));
             
-            // Calculate the new denominator (LCM of current and new denominator)
-            BigInteger commonDen = den.multiply(denominator).divide(den.gcd(denominator));
-            
-            // Adjust the numerators to the new common denominator and add them
-            num = num.multiply(commonDen.divide(den)).add(numerator.multiply(commonDen.divide(denominator)));
-            den = commonDen;
-            
-            // Move to the next part of the expression
-            i = j;
+            numer = numer * den + num * denom;
+            denom *= den;
         }
         
-        // Simplify the fraction
-        BigInteger gcd = num.gcd(den);
-        num = num.divide(gcd);
-        den = den.divide(gcd);
-        
-        return num.toString() + "/" + den.toString();
+        int g = gcd(Math.abs(numer), denom);
+        return (numer / g) + "/" + (denom / g);
+    }
+    
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
