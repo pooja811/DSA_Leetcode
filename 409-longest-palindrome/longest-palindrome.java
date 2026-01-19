78968c2de09a443f42553106a24b0a18ba5850b3
Pooja More <142797724+pooja811@users.noreply.github.com>
2026-01-19 16:16:43 +0530

    Time: 9 ms (21.17%) | Memory: 42.9 MB (86.20%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,23 +1,34 @@
 public class Solution {
+    // public int longestPalindrome(String s) {
+    //     // Initialize a set to keep track of characters with odd frequencies
+    //     HashSet<Character> charSet = new HashSet<>();
+    //     int length = 0;
+    //     for (char c : s.toCharArray()) {
+    //         if (charSet.contains(c)) {
+    //             charSet.remove(c);
+    //             length += 2;
+    //         } else {
+    //             charSet.add(c);
+    //         }
+    //     }
+        
+    //     // If there are any characters left in the set, add 1 to the length for the middle character
+    //     if (!charSet.isEmpty()) {
+    //         length += 1;
+    //     }
+        
+    //     // Return the total length of the longest palindrome
+    //     return length;
+    // }
+
     public int longestPalindrome(String s) {
-        // Initialize a set to keep track of characters with odd frequencies
-        HashSet<Character> charSet = new HashSet<>();
-        int length = 0;
-        for (char c : s.toCharArray()) {
-            if (charSet.contains(c)) {
-                charSet.remove(c);
-                length += 2;
-            } else {
-                charSet.add(c);
-            }
-        }
+        HashMap<Character,Integer> count=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        count.put(s.charAt(i),count.getOrDefault(s.charAt(i),0)+1);
+        int len=0;
+        for(int i:count.values())
+        len+=i/2*2;
         
-        // If there are any characters left in the set, add 1 to the length for the middle character
-        if (!charSet.isEmpty()) {
-            length += 1;
-        }
-        
-        // Return the total length of the longest palindrome
-        return length;
+        return (len<s.length())?len+1:len;
     }
 }
