627e410961a79fdac30a01343c4323eab451a88f
Pooja More <142797724+pooja811@users.noreply.github.com>
2026-04-04 10:49:49 +0530

    Time: 22 ms (71.60%) | Memory: 60.5 MB (10.65%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,22 +1,38 @@
 class Solution {
-    public String decodeCiphertext(String s, int rows) {
-        if (s.isEmpty()) return s;
+    public String decodeCiphertext(String encodedText, int rows) {
+        if(rows == 1)
+            return encodedText;
 
-        int n = s.length();
-        int cols = (n + rows - 1) / rows;
+        int len = encodedText.length();
 
-        StringBuilder res = new StringBuilder();
+        if(len == 0)
+            return encodedText;
 
-        for (int start = 0; start < cols; start++) {
-            int r = 0, c = start;
-            while (r < rows && c < cols) {
-                res.append(s.charAt(r * cols + c));
-                r++; c++;
+        int cols = (int) Math.ceil( (double) len/rows);
+
+        char[][] matrix = new char[rows][cols];
+        char[] encodedArr = encodedText.toCharArray();
+        int e=0;
+
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                matrix[i][j] = encodedArr[e++];
             }
         }
 
-        int i = res.length();
-        while (i > 0 && res.charAt(i - 1) == ' ') i--;
-        return res.substring(0, i);
+        StringBuilder sb = new StringBuilder();
+
+        for (int startCol = 0; startCol < cols; startCol++) {
+            int r = 0;
+            int c = startCol;
+
+            while (r < rows && c < cols) {
+                sb.append(matrix[r][c]);
+                r++;
+                c++;
+            }
+        }
+
+        return sb.toString().stripTrailing();
     }
 }
